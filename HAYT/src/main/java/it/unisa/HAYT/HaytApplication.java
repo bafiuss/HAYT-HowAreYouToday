4a677446c1225dc66c686544cde2b4938c4c6a7b
Fabio Sessa <fsessa02@gmail.com>
2025-01-29 18:10:22 +0100

    initial commit

---
+++ HAYT/src/main/java/it/unisa/HAYT/HaytApplication.java
@@ -0,0 +1,13 @@
+package it.unisa.HAYT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaytApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaytApplication.class, args);
+	}
+
+}
---
+++ HAYT/src/test/java/it/unisa/HAYT/HaytApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unisa.HAYT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaytApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
