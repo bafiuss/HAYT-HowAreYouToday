ef19a61be278ab84e7bb9c038f6bf5df2a10f439
Fabio Sessa <fsessa02@gmail.com>
2025-02-08 14:23:01 +0100

    added encrypted passwords

--- HAYT/src/main/java/it/unisa/HAYT/servicies/UserService.java
+++ HAYT/src/main/java/it/unisa/HAYT/servicies/UserService.java
@@ -3,22 +3,36 @@ package it.unisa.HAYT.servicies;
 import it.unisa.HAYT.dto.PatientSignupDTO;
 import it.unisa.HAYT.entities.UserEntity;
 import it.unisa.HAYT.repositories.UserRepository;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService{
 
     private final UserRepository userRepository;
-    private final BCryptPasswordEncoder passwordEncoder;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
     public boolean emailExists(String email) {
         return userRepository.findByEmail(email).isPresent();
+    }
+
+    public boolean userExists(String email, String password){
+        Optional<UserEntity> userOptional = userRepository.findByEmail(email);
+        if (userOptional.isPresent()) {
+            UserEntity user = userOptional.get();
+
+            if (passwordEncoder.matches(password, user.getPassword())) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public void saveUser(PatientSignupDTO patientSignupDTO) {
@@ -32,6 +46,7 @@ public class UserService{
         user.setLastName(patientSignupDTO.getLastName());
         user.setEmail(patientSignupDTO.getEmail());
         user.setRole("PATIENT");
+        //user.setPassword(patientSignupDTO.getPassword());
         user.setPassword(passwordEncoder.encode(patientSignupDTO.getPassword()));
 
         userRepository.save(user);
