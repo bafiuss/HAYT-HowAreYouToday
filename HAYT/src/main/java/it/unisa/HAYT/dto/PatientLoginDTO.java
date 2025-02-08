7e02f5c4e0c322c105c7bd8181adf370b7b5add5
Fabio Sessa <fsessa02@gmail.com>
2025-02-08 14:22:24 +0100

    added dto for login

---
+++ HAYT/src/main/java/it/unisa/HAYT/dto/PatientLoginDTO.java
@@ -0,0 +1,24 @@
+package it.unisa.HAYT.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PatientLoginDTO {
+
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+
+}
