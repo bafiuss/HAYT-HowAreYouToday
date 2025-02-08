196183af124ac257321420bb4626e4a4c1e86eef
Fabio Sessa <fsessa02@gmail.com>
2025-02-08 14:20:06 +0100

    small fixes

--- HAYT/src/main/java/it/unisa/HAYT/controllers/PatientSignupController.java
+++ HAYT/src/main/java/it/unisa/HAYT/controllers/PatientSignupController.java
@@ -1,15 +1,12 @@
 package it.unisa.HAYT.controllers;
 
 import it.unisa.HAYT.dto.PatientSignupDTO;
-import it.unisa.HAYT.entities.UserEntity;
 import it.unisa.HAYT.servicies.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
--- HAYT/src/main/java/it/unisa/HAYT/repositories/UserRepository.java
+++ HAYT/src/main/java/it/unisa/HAYT/repositories/UserRepository.java
@@ -2,9 +2,11 @@ package it.unisa.HAYT.repositories;
 
 import it.unisa.HAYT.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     Optional<UserEntity> findByEmail(String email);
 }
