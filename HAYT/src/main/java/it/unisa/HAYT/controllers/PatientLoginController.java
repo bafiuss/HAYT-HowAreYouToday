c3a7c1a8c63ea0e5f2828a4e582a624cf86a8736
Fabio Sessa <fsessa02@gmail.com>
2025-02-08 14:23:33 +0100

    added login controller

---
+++ HAYT/src/main/java/it/unisa/HAYT/controllers/PatientLoginController.java
@@ -0,0 +1,43 @@
+package it.unisa.HAYT.controllers;
+
+import it.unisa.HAYT.dto.PatientLoginDTO;
+import it.unisa.HAYT.servicies.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class PatientLoginController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/login")
+    public String patientSignupPage(Model model) {
+        model.addAttribute("hideNavLinks", true);
+        model.addAttribute("patientLoginDTO", new PatientLoginDTO());
+        model.addAttribute("success_login", false);
+        model.addAttribute("failed_login", false);
+        return "login";
+    }
+
+    @PostMapping("/patient-login")
+    public String patientSignup(@Valid @ModelAttribute PatientLoginDTO patientLoginDTO, Model model) {
+
+        if(!userService.userExists(patientLoginDTO.getEmail(),patientLoginDTO.getPassword())) {
+            model.addAttribute("failed_login", true);
+            return "login";
+        }
+
+        model.addAttribute("patientLoginDTO", new PatientLoginDTO());
+        model.addAttribute("success_login", true);
+        model.addAttribute("logged", true);
+
+        return "patient-home";
+    }
+
+}
