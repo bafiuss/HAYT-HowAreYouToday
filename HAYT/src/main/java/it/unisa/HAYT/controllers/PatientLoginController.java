fb5a25974318e5b59aab10899fcd15550307b2c2
Fabio Sessa <fsessa02@gmail.com>
2025-02-11 01:32:38 +0100

    added session for login

--- HAYT/src/main/java/it/unisa/HAYT/controllers/PatientLoginController.java
+++ HAYT/src/main/java/it/unisa/HAYT/controllers/PatientLoginController.java
@@ -1,16 +1,20 @@
 package it.unisa.HAYT.controllers;
 
 import it.unisa.HAYT.dto.PatientLoginDTO;
+import it.unisa.HAYT.entities.UserEntity;
 import it.unisa.HAYT.servicies.UserService;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
+@RequestMapping
+@SessionAttributes("loggedUser")
 public class PatientLoginController {
 
     @Autowired
@@ -20,23 +24,33 @@ public class PatientLoginController {
     public String patientSignupPage(Model model) {
         model.addAttribute("hideNavLinks", true);
         model.addAttribute("patientLoginDTO", new PatientLoginDTO());
-        model.addAttribute("success_login", false);
-        model.addAttribute("failed_login", false);
+        model.addAttribute("successLogin", false);
+        model.addAttribute("failedLogin", false);
+
         return "login";
     }
 
     @PostMapping("/patient-login")
-    public String patientSignup(@Valid @ModelAttribute PatientLoginDTO patientLoginDTO, Model model) {
+    public String patientSignup(@Valid @ModelAttribute("patientLoginDTO") PatientLoginDTO patientLoginDTO,
+                                Model model,
+                                HttpSession session)
+    {
+        String email = patientLoginDTO.getEmail();
+        String password = patientLoginDTO.getPassword();
 
-        if(!userService.userExists(patientLoginDTO.getEmail(),patientLoginDTO.getPassword())) {
-            model.addAttribute("failed_login", true);
-            model.addAttribute("hideNavLinks", true);
+        if(!userService.userExists(email,password) ){
+            model.addAttribute("failedLogin", true);
             return "login";
         }
 
+        UserEntity user = userService.getUser(email);
+        String role = userService.getUserRole(email);
+        session.setAttribute("user", user);
+        session.setAttribute("role", role);
+
         model.addAttribute("patientLoginDTO", new PatientLoginDTO());
-        model.addAttribute("success_login", true);
-        model.addAttribute("logged", true);
+        model.addAttribute("successLogin", true);
+        model.addAttribute("hideNavLinks", false);
 
         return "patient-home";
     }
