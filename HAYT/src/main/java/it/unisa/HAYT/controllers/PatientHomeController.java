e8ce09d950d02eb804657653affe1d54a4d10949
Fabio Sessa <fsessa02@gmail.com>
2025-02-11 01:33:56 +0100

    fixes

--- HAYT/src/main/java/it/unisa/HAYT/controllers/PatientHomeController.java
+++ HAYT/src/main/java/it/unisa/HAYT/controllers/PatientHomeController.java
@@ -1,6 +1,7 @@
 package it.unisa.HAYT.controllers;
 
 import it.unisa.HAYT.dto.PatientLoginDTO;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,9 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class PatientHomeController {
 
     @GetMapping("/patient-home")
-    public String patientSignupPage(Model model) {
+    public String patientHome(Model model) {
         return "patient-home";
     }
-
 
 }
