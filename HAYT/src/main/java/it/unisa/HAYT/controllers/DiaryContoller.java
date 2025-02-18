8050bd47ef2d3dd47a3510098996bb7f5f4c4e91
Fabio Sessa <fsessa02@gmail.com>
2025-02-18 19:49:09 +0100

    small fix

--- HAYT/src/main/java/it/unisa/HAYT/controllers/DiaryContoller.java
+++ HAYT/src/main/java/it/unisa/HAYT/controllers/DiaryContoller.java
@@ -1,17 +1,15 @@
 package it.unisa.HAYT.controllers;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class DiaryContoller {
 
     @GetMapping("/patient-dashboard/diary")
     public String showDiaryPage(Model model){
-        model.addAttribute("hideNavLinks", false);
+        model.addAttribute("hideNavLinks",false);
         return "diary";
     }
 }
