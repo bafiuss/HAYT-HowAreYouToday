f5f96e724db524ed7ac587906d185c91f48b7104
Fabio Sessa <fsessa02@gmail.com>
2025-02-20 15:59:49 +0100

    added psychotherapits entity and DTO

---
+++ HAYT/src/main/java/it/unisa/HAYT/entities/PsychotherapistEntity.java
@@ -0,0 +1,19 @@
+package it.unisa.HAYT.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "psychotherapists")
+public class PsychotherapistEntity extends UserEntity{
+
+    private String location;
+
+}
