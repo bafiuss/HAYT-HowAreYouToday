ec9ea0331e271d035a3ad41b4d6a5c0ad7a0fc95
Fabio Sessa <fsessa02@gmail.com>
2025-02-27 19:45:18 +0100

    added post mapping method

--- HAYT/src/main/java/it/unisa/HAYT/controllers/NewPsychotherapistController.java
+++ HAYT/src/main/java/it/unisa/HAYT/controllers/NewPsychotherapistController.java
@@ -2,10 +2,13 @@ package it.unisa.HAYT.controllers;
 
 import it.unisa.HAYT.entities.PsychotherapistEntity;
 import it.unisa.HAYT.servicies.UserService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -21,5 +24,13 @@ public class NewPsychotherapistController {
         model.addAttribute("psychotherapists", psychotherapistEntityList);
         model.addAttribute("hideNavLinks",false);
         return "new-psychotherapist";
+    }
+
+    @PostMapping("/selected-psychotherapist")
+    public String selectedPsychotherapist(@RequestParam("psychotherapistId") Long psychotherapistId, HttpSession session){
+        Long patientId = (Long) session.getAttribute("userId");
+        userService.psychotherapistAssociation(patientId, psychotherapistId);
+        session.setAttribute("hasTherapist", true);
+        return "patient-dashboard";
     }
 }
