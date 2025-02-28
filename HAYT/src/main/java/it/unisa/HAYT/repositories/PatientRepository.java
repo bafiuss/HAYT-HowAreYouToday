02f359676f61b1b3be33270099114153627874dc
Fabio Sessa <fsessa02@gmail.com>
2025-02-28 14:03:24 +0100

    added queries methods

--- HAYT/src/main/java/it/unisa/HAYT/repositories/PatientRepository.java
+++ HAYT/src/main/java/it/unisa/HAYT/repositories/PatientRepository.java
@@ -1,6 +1,7 @@
 package it.unisa.HAYT.repositories;
 
 import it.unisa.HAYT.entities.PatientEntity;
+import it.unisa.HAYT.entities.PsychotherapistEntity;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -8,6 +9,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -18,5 +20,11 @@ public interface PatientRepository extends JpaRepository<PatientEntity, Long> {
     @Transactional
     @Query("UPDATE PatientEntity p SET p.psychotherapist.id = :psychotherapistId WHERE p.id = :patientId")
     int updatePsychotherapist(@Param("patientId") Long patientId, @Param("psychotherapistId") Long psychotherapistId);
+
+    @Query("SELECT p.psychotherapist FROM PatientEntity p WHERE p.id = :patientId")
+    Optional<PsychotherapistEntity> getAssociatedPsychotherapist(@Param("patientId") Long patientId);
+
+    @Query("SELECT p FROM PsychotherapistEntity p WHERE p.id <> (SELECT p.psychotherapist.id FROM PatientEntity p WHERE p.id = :patientId)")
+    List<PsychotherapistEntity> findPsychotherapistsExcludingPatient(@Param("patientId") Long patientId);
 
 }
--- HAYT/src/main/java/it/unisa/HAYT/repositories/PsychotherapistRepository.java
+++ HAYT/src/main/java/it/unisa/HAYT/repositories/PsychotherapistRepository.java
@@ -1,8 +1,17 @@
 package it.unisa.HAYT.repositories;
 
+import it.unisa.HAYT.entities.PatientEntity;
 import it.unisa.HAYT.entities.PsychotherapistEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
-public interface PsychotherapistRepository extends JpaRepository<PsychotherapistEntity, Long> {}
+public interface PsychotherapistRepository extends JpaRepository<PsychotherapistEntity, Long> {
+
+    Optional<PsychotherapistEntity> findByEmail(String email);
+
+
+}
