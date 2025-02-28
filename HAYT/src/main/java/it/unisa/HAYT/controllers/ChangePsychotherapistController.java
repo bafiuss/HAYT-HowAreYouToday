fdef329dd5b3c09c9542203e8f8f16967c4718b3
Fabio Sessa <fsessa02@gmail.com>
2025-02-28 17:55:38 +0100

    fixes of user saved in session

--- HAYT/src/main/java/it/unisa/HAYT/controllers/ChangePsychotherapistController.java
+++ HAYT/src/main/java/it/unisa/HAYT/controllers/ChangePsychotherapistController.java
@@ -2,6 +2,7 @@ package it.unisa.HAYT.controllers;
 
 import it.unisa.HAYT.entities.PatientEntity;
 import it.unisa.HAYT.entities.PsychotherapistEntity;
+import it.unisa.HAYT.entities.UserEntity;
 import it.unisa.HAYT.servicies.UserService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,12 +21,16 @@ public class ChangePsychotherapistController {
 
     @GetMapping("/patient-dashboard/change-psychotherapist")
     public String showChangePsychotherapistPage(Model model, HttpSession session){
-        Long patientId = (Long) session.getAttribute("userId");
+        PatientEntity patient = (PatientEntity) session.getAttribute("user");
+        Long patientId = patient.getId();
+
         Optional<PsychotherapistEntity> associatedPsychotherapist = userService.getAssociatedPsychotherapist(patientId);
         List<PsychotherapistEntity> psychotherapistsExcludingPatient = userService.findPsychotherapistsExcludingPatient(patientId);
+
         model.addAttribute("psychotherapists", psychotherapistsExcludingPatient);
         model.addAttribute("associatedPsychotherapist", associatedPsychotherapist);
         model.addAttribute("hideNavLinks",false);
+
         return "change-psychotherapist";
     }
 
--- HAYT/src/main/java/it/unisa/HAYT/controllers/NewPsychotherapistController.java
+++ HAYT/src/main/java/it/unisa/HAYT/controllers/NewPsychotherapistController.java
@@ -1,6 +1,8 @@
 package it.unisa.HAYT.controllers;
 
+import it.unisa.HAYT.entities.PatientEntity;
 import it.unisa.HAYT.entities.PsychotherapistEntity;
+import it.unisa.HAYT.entities.UserEntity;
 import it.unisa.HAYT.servicies.UserService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,16 +23,22 @@ public class NewPsychotherapistController {
     @GetMapping("/patient-dashboard/new-psychotherapist")
     public String showNewPsychotherapistPage(Model model){
         List<PsychotherapistEntity> psychotherapistEntityList = userService.getAllPsychotherapists();
+
         model.addAttribute("psychotherapists", psychotherapistEntityList);
         model.addAttribute("hideNavLinks",false);
+
         return "new-psychotherapist";
     }
 
     @PostMapping("/selected-psychotherapist")
-    public String selectedPsychotherapist(@RequestParam("psychotherapistId") Long psychotherapistId, HttpSession session){
-        Long patientId = (Long) session.getAttribute("userId");
+    public String selectedPsychotherapist(@RequestParam("psychotherapistId") Long psychotherapistId, HttpSession session, Model model){
+        PatientEntity patient = (PatientEntity) session.getAttribute("user");
+        Long patientId = patient.getId();
         userService.psychotherapistAssociation(patientId, psychotherapistId);
+
         session.setAttribute("hasTherapist", true);
+        model.addAttribute("hideNavLinks",false);
+
         return "patient-dashboard";
     }
 }
