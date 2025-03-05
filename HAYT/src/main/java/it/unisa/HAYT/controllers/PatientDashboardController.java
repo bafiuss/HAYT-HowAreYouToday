056784e0b56b25d1fdf6a340f70a3b62eec3a0da
Fabio Sessa <fsessa02@gmail.com>
2025-03-05 17:29:45 +0100

    small fixes

--- HAYT/src/main/java/it/unisa/HAYT/controllers/PatientDashboardController.java
+++ HAYT/src/main/java/it/unisa/HAYT/controllers/PatientDashboardController.java
@@ -1,39 +1,15 @@
 package it.unisa.HAYT.controllers;
 
-import it.unisa.HAYT.entities.PatientEntity;
-import jakarta.servlet.http.HttpSession;
-import it.unisa.HAYT.servicies.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.Optional;
-
 @Controller
 public class PatientDashboardController {
 
-    @Autowired
-    private UserService userService;
-
     @GetMapping("/patient-dashboard")
-    public String showPatientDashboard(@AuthenticationPrincipal UserDetails userDetails, Model model, HttpSession session) {
-        if (userDetails != null) {
-            String username = userDetails.getUsername();
-            Optional<PatientEntity> patientOptional = userService.getPatient(username);
-
-            if (patientOptional.isPresent()) {
-                PatientEntity patient = patientOptional.get();
-
-                boolean hasTherapist = patient.getPsychotherapist() != null;
-                session.setAttribute("hasTherapist", hasTherapist);
-            }
-        }
-
+    public String showPatientDashboardPage(Model model) {
         model.addAttribute("hideNavLinks", false);
-        model.addAttribute("currentPage", "patient-dashboard");
 
         return "patient-dashboard";
     }
