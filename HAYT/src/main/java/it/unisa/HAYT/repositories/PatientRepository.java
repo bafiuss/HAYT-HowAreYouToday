a6e246a7305600905e79cd891b20a1445712213f
Fabio Sessa <fsessa02@gmail.com>
2025-03-06 13:39:09 +0100

    added number of patient associated method

--- HAYT/src/main/java/it/unisa/HAYT/repositories/PatientRepository.java
+++ HAYT/src/main/java/it/unisa/HAYT/repositories/PatientRepository.java
@@ -16,15 +16,8 @@ import java.util.Optional;
 public interface PatientRepository extends JpaRepository<PatientEntity, Long> {
     Optional<PatientEntity> findByEmail(String email);
 
-    @Modifying
-    @Transactional
-    @Query("UPDATE PatientEntity p SET p.psychotherapist.id = :psychotherapistId WHERE p.id = :patientId")
-    int updatePsychotherapist(@Param("patientId") Long patientId, @Param("psychotherapistId") Long psychotherapistId);
+    @Query("SELECT Count(*) FROM PatientEntity p WHERE p.psychotherapist.id = :psychotherapistId")
+    int numberOfPatientsAssociated(@Param("psychotherapistId") Long psychotherapistId);
 
-    @Query("SELECT p.psychotherapist FROM PatientEntity p WHERE p.id = :patientId")
-    Optional<PsychotherapistEntity> getAssociatedPsychotherapist(@Param("patientId") Long patientId);
-
-    @Query("SELECT p FROM PsychotherapistEntity p WHERE p.id <> (SELECT p.psychotherapist.id FROM PatientEntity p WHERE p.id = :patientId)")
-    List<PsychotherapistEntity> findPsychotherapistsExcludingPatient(@Param("patientId") Long patientId);
 
 }
