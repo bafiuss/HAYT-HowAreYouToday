03052966d0bcd7a911fa4924a6ecec41a06d107e
Fabio Sessa <fsessa02@gmail.com>
2025-03-17 00:41:19 +0100

    added message entity

---
+++ HAYT/src/main/java/it/unisa/HAYT/entities/MessageEntity.java
@@ -0,0 +1,36 @@
+package it.unisa.HAYT.entities;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class MessageEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id", nullable = false)
+    private UserEntity sender;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_id", nullable = false)
+    private UserEntity receiver;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private LocalDateTime timestamp;
+
+}
+
