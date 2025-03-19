920f665811d460cff249bfdff37cca9201bc23c9
Fabio Sessa <fsessa02@gmail.com>
2025-03-19 15:29:41 +0100

    small fixes

--- HAYT/src/main/java/it/unisa/HAYT/controllers/PatientChatController.java
+++ HAYT/src/main/java/it/unisa/HAYT/controllers/PatientChatController.java
@@ -1,16 +1,21 @@
 package it.unisa.HAYT.controllers;
 
+import it.unisa.HAYT.dto.AppointmentDTO;
 import it.unisa.HAYT.entities.*;
+import it.unisa.HAYT.servicies.AppointmentService;
 import it.unisa.HAYT.servicies.MessageService;
 import it.unisa.HAYT.servicies.UserService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
@@ -22,6 +27,9 @@ public class PatientChatController {
 
     @Autowired
     private MessageService messageService;
+
+    @Autowired
+    private AppointmentService appointmentService;
 
     @GetMapping("/patient-dashboard/chat")
     public String showPatientChatPage(Model model, HttpSession session){
@@ -37,6 +45,17 @@ public class PatientChatController {
         model.addAttribute("hideNavLinks",false);
 
         return "patient-chat";
+    }
+
+    @PostMapping("/save-appointment")
+    public ResponseEntity<String> createAppointment(@RequestBody AppointmentDTO request, Model model) {
+        String response = appointmentService.saveAppointment(request);
+
+        if (response.equals("Appointment created successfully")) {
+            return ResponseEntity.ok(response);
+        } else {
+            return ResponseEntity.badRequest().body(response);
+        }
     }
 
     @MessageMapping("/sendMessage")
