242a946b7f5175d967e60925fc03bc0a780aa681
Fabio Sessa <fsessa02@gmail.com>
2025-03-24 14:36:27 +0100

    added controller with appointments methods

---
+++ HAYT/src/main/java/it/unisa/HAYT/controllers/PatientAppointmentController.java
@@ -0,0 +1,39 @@
+package it.unisa.HAYT.controllers;
+
+import it.unisa.HAYT.dto.AppointmentDTO;
+import it.unisa.HAYT.entities.AppointmentEntity;
+import it.unisa.HAYT.entities.PatientEntity;
+import it.unisa.HAYT.entities.PsychotherapistEntity;
+import it.unisa.HAYT.servicies.AppointmentService;
+import it.unisa.HAYT.servicies.UserService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class PatientAppointmentController {
+
+    @Autowired
+    private AppointmentService appointmentService;
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping ("/appointments")
+    public List<AppointmentEntity> getAllAppointments(HttpSession session) {
+        PatientEntity patient = (PatientEntity) session.getAttribute("user");
+        PsychotherapistEntity psychotherapistAssociated = userService.getPsychotherapistAssociated(patient.getId());
+
+        return appointmentService.getAppointmentsByPatientAndTherapist(patient.getId(), psychotherapistAssociated.getId());
+    }
+
+    @PostMapping("/save-appointment")
+    public String createAppointment(@RequestBody AppointmentDTO request) {
+        appointmentService.saveAppointment(request);
+
+        return "patient-chat";
+    }
+}
+
