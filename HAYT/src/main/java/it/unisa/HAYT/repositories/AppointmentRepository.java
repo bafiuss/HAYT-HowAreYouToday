f87215c4184b070a84d18aad722adc27d14ce380
Fabio Sessa <fsessa02@gmail.com>
2025-03-24 14:37:32 +0100

    added appointments method between patient and therapist

--- HAYT/src/main/java/it/unisa/HAYT/repositories/AppointmentRepository.java
+++ HAYT/src/main/java/it/unisa/HAYT/repositories/AppointmentRepository.java
@@ -2,7 +2,13 @@ package it.unisa.HAYT.repositories;
 
 import it.unisa.HAYT.entities.AppointmentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface AppointmentRepository extends JpaRepository<AppointmentEntity, Long> {
+    List<AppointmentEntity> findByPatientIdAndPsychotherapistId(Long patientId, Long psychotherapistId);
 }
+
 
