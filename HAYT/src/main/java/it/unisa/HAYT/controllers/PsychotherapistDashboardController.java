1c46581888a069af939a7f08266d62599ede5f39
Fabio Sessa <fsessa02@gmail.com>
2025-03-26 18:57:06 +0100

    small fixes

--- HAYT/src/main/java/it/unisa/HAYT/controllers/PsychotherapistDashboardController.java
+++ HAYT/src/main/java/it/unisa/HAYT/controllers/PsychotherapistDashboardController.java
@@ -1,13 +1,17 @@
 package it.unisa.HAYT.controllers;
 
+import it.unisa.HAYT.entities.AppointmentEntity;
 import it.unisa.HAYT.entities.PatientEntity;
+import it.unisa.HAYT.entities.PsychotherapistEntity;
 import it.unisa.HAYT.entities.UserEntity;
+import it.unisa.HAYT.servicies.AppointmentService;
 import it.unisa.HAYT.servicies.UserService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -16,6 +20,7 @@ public class PsychotherapistDashboardController {
 
     @Autowired
     private UserService userService;
+
 
     @GetMapping("/psychotherapist-dashboard")
     public String showPsychotherapistDashboardPage(Model model, HttpSession session) {
@@ -31,6 +36,5 @@ public class PsychotherapistDashboardController {
 
         return "psychotherapist-dashboard";
     }
-
 
 }
