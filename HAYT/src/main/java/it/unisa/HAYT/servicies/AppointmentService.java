b888f26437ee9f3f5e78655c7fff152266672eb0
Fabio Sessa <fsessa02@gmail.com>
2025-03-31 14:35:33 +0200

    added converter to dto method

--- HAYT/src/main/java/it/unisa/HAYT/servicies/AppointmentService.java
+++ HAYT/src/main/java/it/unisa/HAYT/servicies/AppointmentService.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class AppointmentService {
@@ -41,6 +42,25 @@ public class AppointmentService {
 
     public List<AppointmentEntity> getAppointmentsByPatient(Long psychotherapistId){
         return appointmentRepository.findByPsychotherapistId(psychotherapistId);
+    }
+
+    public List<AppointmentDTO> getAllPsychotherapistAppointments(Long psychotherapistId) {
+        List<AppointmentEntity> appointments = appointmentRepository.findByPsychotherapistId(psychotherapistId);
+        return appointments.stream().map(this::convertPsychotherapistAppointmentsToDTO).collect(Collectors.toList());
+    }
+
+    private AppointmentDTO convertPsychotherapistAppointmentsToDTO(AppointmentEntity appointment) {
+        AppointmentDTO dto = new AppointmentDTO();
+
+        dto.setTitle(appointment.getTitle());
+        dto.setDateTime(appointment.getDateTime());
+        dto.setDescription(appointment.getDescription());
+        dto.setPatientId(appointment.getPatient().getId());
+        dto.setPsychotherapistId(appointment.getPsychotherapist().getId());
+        dto.setPatientFirstName(appointment.getPatient().getFirstName());
+        dto.setPatientLastName(appointment.getPatient().getLastName());
+
+        return dto;
     }
 
 }
