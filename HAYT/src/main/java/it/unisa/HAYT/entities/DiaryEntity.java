a33d96b694017ee03a13d2814273c86e9219b6f2
Fabio Sessa <fsessa02@gmail.com>
2025-05-14 13:32:28 +0200

    added diary entity

---
+++ HAYT/src/main/java/it/unisa/HAYT/entities/DiaryEntity.java
@@ -0,0 +1,32 @@
+package it.unisa.HAYT.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "diary_thoughts")
+public class DiaryEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private String mood;
+
+    private String sentiment;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+}
