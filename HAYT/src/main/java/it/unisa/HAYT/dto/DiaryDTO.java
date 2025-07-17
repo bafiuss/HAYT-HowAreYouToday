986a8b4847261fdb480a174c5f3589ccb91584d3
Fabio Sessa <fsessa02@gmail.com>
2025-07-17 19:57:57 +0200

    added DiaryDTO constructors

--- HAYT/src/main/java/it/unisa/HAYT/dto/DiaryDTO.java
+++ HAYT/src/main/java/it/unisa/HAYT/dto/DiaryDTO.java
@@ -1,8 +1,14 @@
 package it.unisa.HAYT.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Setter
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class DiaryDTO {
 
     private String title;
